80b9b0344fd849e1b9bf4e06bdbee0b76503562d
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-05-03 17:13:31 +0200

    login page and logic completed

--- Controllers/auth.php
+++ Controllers/auth.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
 include __DIR__ . "/../Models/user.php";
+$loginfailed = '<div class="mt-4 p-1 bg-danger rounded-1">E-mail o password errate, riprovare</div>';
 
-var_dump($users);
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+    $logged = array_filter($users, function ($user) use ($email, $pass){
+        return $user['email'] == $email && $user['password'] == $pass;
+    });
+    // var_dump($logged);
+    if(count($logged) > 0){
+        $_SESSION['logged'] = true;
+        header ('Location: index.php');
+    } else{
+        echo $loginfailed;
+    }
+};
--- loginpage.php
+++ loginpage.php
@@ -1,4 +1,7 @@
 <?php
+include __DIR__ . "/Controllers/auth.php";
+
+
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +16,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link grity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 
-    <script src="js/script.js" type="text/javascript" defer></script>
+    
     <title>Login</title>
 
 </head>
@@ -23,17 +26,18 @@
 <body>
     <div class="container login">
         <div class="d-flex justify-content-center align-items-center">            
-            <form>
+            <form id="loginform" action="loginpage.php" method="POST">
             <h1 class="text-center mt-4">Please log-in</h1>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control" name="email">
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Password</label>
-                    <input type="password" class="form-control">
+                    <input type="password" class="form-control" name="password">
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
+                
             </form>
         </div>
     </div>
